1cbceaa395ebe6c13147483fe866d3cfb70226e3
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-07-28 23:10:08 +0530

    2

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-gsap.to("#Gokhul",{
-    x:1000,
-    duration:2,
+gsap.from(".page1 .box",{
+    scale:0,
     delay:1,
+    duration:1,
+    rotate:360,
+    scrollTrigger:{
+        trigger:".page1 .box",
+        scroller:"body",
+        markers:true,
+        start:"top 50%",
 
-
-    
-}) 
-gsap.from("#Gokhul1",{
-
-    x:1000,
-    y:1000,
+        scrub:2
+    }
+})
+gsap.from(".page2 h1",{
+    opacity:0,
+    x:500,
     duration:2,
-    delay:1,
+    scrollTrigger:{
+        trigger:".page2 h1",
+        scroller:"body",
+            // markers:true,
+            // start:"top 60%"
+    }
 
+})
+gsap.from(".page2 h2",{
+    opacity:0,
+    x:-500,
+    duration:2,
+    scrollTrigger:{
+        trigger:".page2 h2",
+        scroller:"body",
+        markers:true,
+        start:"top 50%",
 
-    
-}) 
+        scrub:2
+    }
+
+})
+// gsap.from(".page2 .box",{
+//     scale:0,
+//     duration:1,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:".page2 .box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+gsap.from(".page3 .box",{
+    scale:0,
+    // delay:1,
+    duration:1,
+    rotate:360,
+    scrollTrigger:{
+        trigger:".page3 .box",
+        scroller:"body",
+            // markers:true,
+            // start:"top 60%"
+    }
+})
